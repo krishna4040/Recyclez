00065c5d0fc2ffe794c5039d1b0c31bc069c242c
D@reK@r@n18 <xpresskaran98@gmail.com>
2024-01-03 17:41:24 +0530

    updated nav

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -1,31 +1,36 @@
 import Hamburger from "./Hamburger";
 import CloseIcon from "./CloseIcon";
 import { useState } from "react";
-import { NavLink, useNavigate } from 'react-router-dom'
-import { setToken } from '../../store/slice/authSlice'
-
-import imgSrc from '../../assets/bg.png'
+import { NavLink, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import toast from "react-hot-toast";
+import { setToken, LOGO } from "../../RootImport.js";
 
 const Nav = () => {
   const [navLinkVisibility, setNavLinkVisibility] = useState(false);
-  const user = useSelector(state => state.user);
+  const user = useSelector((state) => state.user);
 
   const Links = [
-    { title: 'Home', linkto: '/' },
-    { title: 'Profile', linkto: '/profile' },
-    { title: `${user.role === 'Receiver' ? 'Request waste' : 'Add Waste'}`, linkto: `${user.role === 'Receiver' ? '/waste/request-waste' : '/waste/add-waste'}` },
-  ]
+    { title: "Home", linkto: "/user/dashboard" },
+    { title: "Profile", linkto: "/user/profile" },
+    {
+      title: `${user.role === "Receiver" ? "Request waste" : "Add Waste"}`,
+      linkto: `${
+        user.role === "Receiver"
+          ? "/user/waste/request-waste"
+          : "/user/waste/add-waste"
+      }`,
+    },
+  ];
 
   const dispacth = useDispatch();
   const navigate = useNavigate();
 
   const logoutHandler = () => {
     dispacth(setToken(null));
-    navigate('/');
-    toast.success('Logout Successfull');
-  }
+    navigate("/");
+    toast.success("Logout Successfull");
+  };
 
   const linkWrapper = Links.map((value, index) => {
     return (
@@ -72,11 +77,24 @@ const Nav = () => {
           <div className="p-1 m-1">
             <ul className="flex flex-col items-center justify-center p-1 m-1">
               {linkWrapper}
-              <p onClick={logoutHandler} className="p-1 m-1 text-xl text-center text-white capitalize cursor-pointer font-navLinks">Logout</p>
+              <p
+                onClick={logoutHandler}
+                className="p-1 m-1 text-xl text-center text-white capitalize cursor-pointer font-navLinks"
+              >
+                Logout
+              </p>
             </ul>
             <div className="flex items-center justify-center p-1 m-1">
               <div className="flex flex-col items-center justify-center p-1 m-1">
-                <img src={imgSrc} alt="Logo" className="w-1/2 m-1 rounded-lg" />
+                <img
+                  src={LOGO}
+                  alt="Logo"
+                  className="w-1/2 m-1 rounded-lg"
+                  draggable="false"
+                  onClick={() => {
+                    navigate("/");
+                  }}
+                />
                 <span className="p-1 m-1 text-white text-md">
                   &copy; Team MERN
                 </span>
