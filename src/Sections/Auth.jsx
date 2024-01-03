5bad85aa9fd889b95e10348b424b444e9d801f32
D@reK@r@n18 <xpresskaran98@gmail.com>
2024-01-03 15:08:15 +0530

    changes in auth page

--- src/Sections/Auth.jsx
+++ src/Sections/Auth.jsx
@@ -1,30 +1,73 @@
 import { useState } from "react";
-import BG_IMG from "../assets/bg2.jpg";
-import Login from '../components/Auth/Login'
-import Signup from '../components/Auth/Signup'
+import Login from "../components/Auth/Login";
+import Signup from "../components/Auth/Signup";
+import OAuth from "../components/Auth/OAuth";
 
 const Auth = () => {
   const [signupstatus, setSignupstatus] = useState(true);
 
   return (
     <>
-      <section className="w-full h-screen bg-[#000000] p-4">
-        <div className="w-full h-full bg-white bg-no-repeat bg-cover rounded-md" style={{ background: `url(${BG_IMG})` }}>
-          <div>
-            <div className="flex flex-row items-center justify-center p-1 m-1">
-              <button
-                className="p-1 m-2 text-lg text-black capitalize border-2 border-black border-solid rounded-md font-montserrat"
-                onClick={() => {
-                  setSignupstatus(!signupstatus);
-                }}
-              >
-                {signupstatus ? (<p>Login instead</p>) : (<p>Signup instead</p>)}
-              </button>
-            </div>
-            <div className="w-full h-[85vh] mt-3 p-1 flex items-center justify-center flex-col">
-              {signupstatus ? <Signup setSignupstatus={setSignupstatus} /> : <Login />}
-            </div>
+      <section className="w-full min-h-screen bg-gradient-to-b from-white via-yellow-300 to-orange-400 flex flex-col items-center">
+        <div className="flex flex-col items-center justify-between">
+          <h1 className="font-bold text-4xl m-2 text-black">
+            Welcome to
+            <span className="font-bold bg-gradient-to-r from-rose-400 to-red-500 text-transparent bg-clip-text text-6xl capitalize mx-2">
+              Recyclez
+            </span>
+          </h1>
+          <h2 className="text-sm m-2 text-slate-800 font-light">
+            An end-to-end Waste Management System
+          </h2>
+        </div>
+        <div className="m-3 w-full flex flex-col items-center">
+          {signupstatus ? (
+            <>
+              <h3 className="text-2xl font-black text-slate-800 m-2">
+                Don{`'`}t have an Account ?
+              </h3>
+              <Signup />
+              <p className="font-light text-base text-slate-600 m-2">
+                Already have an Account.{" "}
+                <span
+                  className="text-black font-black cursor-pointer hover:underline text-lg"
+                  onClick={() => {
+                    setSignupstatus((prev) => !prev);
+                  }}
+                >
+                  Login
+                </span>
+              </p>
+            </>
+          ) : (
+            <>
+              <h3 className="text-2xl font-black text-slate-800 m-2">
+                Already have an Account.
+              </h3>
+              <Login />
+              <p className="font-light text-base text-slate-600 m-2">
+                Don{`'`}t have an Account ?{" "}
+                <span
+                  className="text-black font-black cursor-pointer hover:underline text-lg"
+                  onClick={() => {
+                    setSignupstatus((prev) => !prev);
+                  }}
+                >
+                  Sign Up
+                </span>
+              </p>
+            </>
+          )}
+          <div className="w-full flex flex-row items-center justify-between max-w-md">
+            <span className="w-full h-[0.51px] bg-slate-600 mx-2"></span>
+            <span className="text-base text-slate-600 font-light mx-1 mb-1">
+              or
+            </span>
+            <span className="w-full h-[0.51px] bg-slate-600 mx-2"></span>
           </div>
+        </div>
+        <div className="flex flex-col">
+          <OAuth />
         </div>
       </section>
     </>
